1ad781738228a7c3a6ba15f0815cf9eb7aabe113
Dmitry <wodi220500@gmail.com>
2020-10-07 20:54:23 +0300

    Initial Commit

---
+++ Companies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Companies
+//
+//  Created by Dmitriy Chernov on 07.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
